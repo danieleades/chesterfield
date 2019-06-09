a4422898509cced18d1fbb157663efe366b029ab
Daniel Eades <danieleades@hotmail.com>
2019-06-09 09:13:22 +0100

    add link to docs to README.md

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
 //! An ergonomic, strongly typed CouchDB client in Rust.
 //!
 //! This library includes both synchronous and asynchronous APIs for the programmer who wants to have it all.
+//! 
+//! [see the documentation](https://docs.rs/chesterfield/0.0.1/chesterfield/)
+//! *(note that the docs.rs documentation might be lagging behind master)*
 //!
 //! Couldn't find a decent, maintained CouchDB client in Rust. Also I wanted async. So i rolled my own.
 //!
 //! This is still in active development, in the sense that I add things when I need them, and fix bugs when they affect
 //! me directly.
-//!
-//! Until this gets slightly closer to stable the updates to the release on Crates.io will be a lot more sporadic than the github updates.
 //!
 //! Would be thrilled to have a couple more sets of eyes and keyboards chipping away at this. climb aboard.
 
